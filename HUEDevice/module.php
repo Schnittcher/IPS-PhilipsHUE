7a51f435e8d3c58ff16ce73cbace3aa1edc68c00
Kai Schnittcher <kai.schnittcher@gmail.com>
2021-04-15 11:41:54 +0200

    Fix Color

--- HUEDevice/module.php
+++ HUEDevice/module.php
@@ -395,6 +395,7 @@ class HUEDevice extends IPSModule
 
         //If $Value Hex Color convert to Decimal
         if (preg_match('/^#[a-f0-9]{6}$/i', strval($Value))) {
+            $Value = ltrim($Value['hex'], '#');
             $Value = hexdec($Value);
         }
 
